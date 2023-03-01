223bcde026b29241411c93f3e58efdead8bf57c8
Jun Ando <jun.ando@amd.com>
2023-03-01 22:57:30 +0900

    Fixed start.sh

--- start.sh
+++ start.sh
@@ -8,10 +8,11 @@ cd $dir
 tmux new-session -d -s demo-session "$dir/docker/run.sh --name demo-ml --port 8554,5555,5556 --card u5 ./start_ml.sh"
 tmux select-layout even-horizontal
 
+sleep 3
+ip_ml=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' demo-ml)
+
 tmux split-window -fv
 tmux send "$dir/docker/enter.sh --name demo-ml ./start_rtsp.sh" ENTER
-
-ip_ml=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' demo-ml)
 
 tmux split-window -fv
 tmux send "$dir/docker/run.sh --name demo-video --port 8555 --env ML_SERVER_IP=$ip_ml,RTSP_SERVER_IP=$ip_ml --card u30 ./start_demo.sh" ENTER
